2d4eaa64226630b8373bdd425d9f9b27bd4942be
Gilberto Barros <gilbertogne@hotmail.com>
2026-09-20 10:46:22 +0100

    updates from vs

--- PlayerTwoTracker/PlayerTwoTracker.Tests/obj/Debug/net10.0/PlayerTwoTracker.Tests.AssemblyInfo.cs
+++ PlayerTwoTracker/PlayerTwoTracker.Tests/obj/Debug/net10.0/PlayerTwoTracker.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayerTwoTracker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7734807b76073ed85fad0c39d7b46498dbe6b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4941531f6d510a455eae832f54972e0d9123e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerTwoTracker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerTwoTracker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
